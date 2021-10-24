9fc5641783c3c648c987820f1f6392279468a119
NZambare <57184218+nzambare@users.noreply.github.com>
2021-10-24 13:22:45 +0530

    Added two more options for searching for a variable: binary search and interpolation search.

---
+++ structures/sorted_list.py
@@ -0,0 +1,83 @@
+import time
+
+
+class SortedList:
+    def __init__(self):
+        self.array = []
+        self.iter_index = 0
+
+    def append(self, val):
+        if len(self.array) == 0:
+            self.array.append(val)
+        elif len(self.array) == 1:
+            if self.array[0] < val:
+                self.array.append(val)
+            else:
+                self.array.insert(0, val)
+        else:
+            self.array.insert(self.find_add(val, 0, len(self.array)-1), val)
+
+    def find_add(self, val, low, high):
+        if self.array[low] < val < self.array[high]:
+            pos = low + (((val - self.array[low]) * (high - low)) // (self.array[high] - self.array[low]))
+            if self.array[pos] <= val <= self.array[pos+1]:
+                return pos+1
+            if self.array[pos-1] <= val <= self.array[pos]:
+                return pos
+            if self.array[pos] < val:
+                return self.find_add(val, pos+1, high)
+            if self.array[pos] > val:
+                return self.find_add(val, low, pos-1)
+        if val <= self.array[low]:
+            return low
+        else:
+            return high+1
+
+    def binary_search(self, low, high, val):
+        if low <= high and self.array[low] <= val <= self.array[high]:
+            pos = low + (high-low)//2
+            if self.array[pos] == val:
+                return pos
+            if self.array[pos] < val:
+                return self.binary_search(pos+1, high, val)
+            else:
+                return self.binary_search(low, pos-1, val)
+        return -1
+
+    def interpolation_search(self, low, high, val):
+        if low <= high and self.array[low] <= val <= self.array[high]:
+            pos = low + (((-(self.array[low]-val)) * (high - low)) // (self.array[high] - self.array[low]))
+            if self.array[pos] == val:
+                return pos
+            if self.array[pos] < val:
+                return self.interpolation_search(pos+1, high, val)
+            else:
+                return self.interpolation_search(low, pos-1, val)
+        return -1
+
+    def __getitem__(self, item):
+        return self.array[item]
+
+    def __setitem__(self, key, value):
+        self.array[key] = value
+
+    def __delitem__(self, key):
+        del self.array[key]
+
+    def __iter__(self):
+        self.iter_index = 0
+        return self
+
+    def __next__(self):
+        if self.iter_index < len(self.array):
+            val = self.array[self.iter_index]
+            self.iter_index += 1
+            return val
+        else:
+            raise StopIteration
+
+    def __len__(self):
+        return len(self.array)
+
+    def clear(self):
+        self.array.clear()
--- vars.py
+++ vars.py
@@ -1,9 +1,15 @@
 from errors import PyscriptNameError
+from structures.sorted_list import SortedList
+import time
 
 
-global_vars = []
+global_vars = SortedList()
 cached = []
 current_scope_pointer = -1
+current_var = None
+current_index = -1
+# 0: linear, 1: binary, 2: interpolation
+search_mode = 2
 
 
 class Variable:
@@ -11,6 +17,9 @@ class Variable:
         self.value = value
         self.name = name
         self.readonly = readonly
+        self.name_val = 0
+        for letter in self.name:
+            self.name_val += ord(letter)
 
     def __repr__(self):
         value = self.value
@@ -21,17 +30,89 @@ class Variable:
         else:
             return f"readonly {self.name} = {value}"
 
+    def __lt__(self, other):
+        return self.name < other
+
+    def __gt__(self, other):
+        return self.name > other
+
+    def __le__(self, other):
+        return self.name <= other
+
+    def __ge__(self, other):
+        return self.name >= other
+
+    def __eq__(self, other):
+        return self.name == other
+
+    def __ne__(self, other):
+        return self.name != other
+
+    def __add__(self, other):
+        if type(other) == Variable:
+            op_val = other.name_val
+        elif type(other) == str:
+            op_val = 0
+            for letter in other:
+                op_val += ord(letter)
+        return self.name_val + op_val
+
+    def __sub__(self, other):
+        if type(other) == Variable:
+            op_val = other.name_val
+        elif type(other) == str:
+            op_val = 0
+            for letter in other:
+                op_val += ord(letter)
+        return self.name_val - op_val
+
+    def __mul__(self, other):
+        if type(other) == Variable:
+            op_val = other.name_val
+        elif type(other) == str:
+            op_val = 0
+            for letter in other:
+                op_val += ord(letter)
+        return self.name_val * op_val
+
 
 def create_var(name, value, readonly=False):
     global_vars.append(Variable(name, value, readonly))
     return global_vars[-1].value
 
 
+def search_for_var(name):
+    global current_var, current_index
+    if search_mode == 0:
+        i = 0
+        for var in global_vars:
+            if var == name:
+                current_var = var
+                current_index = i
+                return
+            i += 1
+        current_var = -1
+    elif search_mode == 1:
+        i = global_vars.binary_search(0, len(global_vars)-1, name)
+        if i > -1:
+            current_var = global_vars[i]
+            current_index = i
+            return
+        current_var = -1
+    elif search_mode == 2:
+        i = global_vars.interpolation_search(0, len(global_vars)-1, name)
+        if i > -1:
+            current_var = global_vars[i]
+            current_index = i
+            return
+        current_var = -1
+
+
 def set_var(name, value, readonly=False):
-    for var in global_vars:
-        if var.name == name:
-            var.value = value
-            return var.value
+    search_for_var(name)
+    if current_var != -1:
+        current_var.value = value
+        return current_var.value
     return create_var(name, value, readonly)
 
 
@@ -54,18 +135,17 @@ def get_var_from_previous_scope(name):
 
 
 def remove_var(name):
-    i = 0
-    while i < len(global_vars):
-        if global_vars[i].name == name:
-            val = global_vars[i]
-            del global_vars[i]
-            return val
-        i += 1
+    global current_var
+    search_for_var(name)
+    if current_var != -1:
+        val = global_vars[current_index]
+        del global_vars[current_index]
+        return val
     PyscriptNameError(f"variable '{name}' was referenced before assignment", True)
 
 
 def start_new_scope():
-    cached.append([])
+    cached.append(SortedList())
     for var in global_vars:
         cached[-1].append(Variable(var.name, var.value, var.readonly))
     global_vars.clear()
@@ -82,10 +162,19 @@ def revert_from_scope():
 def push_to_previous_cache(name):
     removed = remove_var(name)
     create_var(removed.name, removed.value, removed.readonly)
-    for var in cached[-1]:
-        if var.name == name:
-            var.value = removed.value
-            return
+    if search_mode == 0:
+        for var in cached[-1]:
+            if var.name == name:
+                var.value = removed.value
+                return
+    elif search_mode == 1:
+        i = cached[-1].binary_search(0, len(cached[-1])-1, name)
+        if i > -1:
+            cached[-1][i].value = removed.value
+    elif search_mode == 2:
+        i = cached[-1].interpolation_search(0, len(cached[-1]) - 1, name)
+        if i > -1:
+            cached[-1][i].value = removed.value
     cached[-1].append(removed)
 
 
