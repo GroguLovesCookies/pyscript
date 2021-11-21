7693b223885a2804ac947ac961d477317d181212
NZambare <57184218+nzambare@users.noreply.github.com>
2021-11-21 14:37:50 +0530

    Realised my unittests were out and my filename was exactly opposite of what it should be.

--- unittests/bracket_test.py
+++ unittests/bracket_test.py
@@ -6,10 +6,6 @@ class BracketTest(unittest.TestCase):
     def test_closing(self):
         self.assertEqual(4, get_closing(read("(2*2)")))
 
-    def test_brackets(self):
-        print(prep_unary())
-        self.assertEqual(2, 2)
-
 
 if __name__ == '__main__':
     unittest.main()
